ecd0fc9c3b35b83a79dfcae385c5ce28da7eba91
sziliati <mail@sziliati.com>
2026-05-31 22:30:59 +0200

    actuator

--- local-run/app-start.sh
+++ local-run/app-start.sh
@@ -5,5 +5,6 @@ if [ $? -eq 0 ]
 then
     java -Xmx512m -Dserver.port=8082 \
     -Dapi.message.url=http://localhost:8081 \
+    -Dmanagement.server.port=9082 \
     -jar target/cloudnative-api-1.0.0-SNAPSHOT.jar
 fi
